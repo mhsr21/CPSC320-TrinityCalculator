54d77165c976a769e1ceaae83f84928bea5fde48
mhsr21 <chris.d.d.yi@gmail.com>
2023-09-10 23:23:04 -0400

    build

--- src/test/java/trinity/LinkedListTest.java
+++ src/test/java/trinity/LinkedListTest.java
@@ -1,4 +1,16 @@
+package trinity;
+
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
 class LinkedListTest {
-  
+    @Test
+    void append() {
+        LinkedList<String> ll1 = new LinkedList<String>();
+        ll1.append("Test");
+        assertEquals("Test", ll1.delete());
+        
+    }
+    
+
 }
