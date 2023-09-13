2e2eca1823c1f8b6edd32bf350547f69823c0688
mhsr21 <chris.d.d.yi@gmail.com>
2023-09-13 18:56:31 -0400

    add gson dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,9 @@ repositories {
 }
 
 dependencies {
+    // https://mvnrepository.com/artifact/com.google.code.gson/gson
+    implementation("com.google.code.gson:gson:2.10.1")
+
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
